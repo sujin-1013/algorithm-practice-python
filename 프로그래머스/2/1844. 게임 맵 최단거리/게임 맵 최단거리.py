7885b927fcc84ed0f81895f5301aeccbda999901
Sujin Choi <65237193+sujin-1013@users.noreply.github.com>
2026-04-23 22:50:04 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.48 ms, Memory: 9.48 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,31 +1,36 @@
 from collections import deque
 
-
 def solution(maps):
     
-    n,m = len(maps), len(maps[0])
-    visited = [[False for _ in range(m)] for _ in range(n)]
-    q = deque()
-    directions = [(-1,0), (1,0), (0,-1), (0,1)]
+    start = (0,0)
+    n = len(maps)
+    m = len(maps[0])
+    goal = (n-1, m-1)
     
-    idx = 1
-    pos = (0,0,idx)
-    visited[pos[0]][pos[1]] = True
-    q.append(pos)
+    visited = [[False for _ in range(m)] for _ in range(n)]   
+    dist = [[0 for _ in range(m)] for _ in range(n)]
+    
+    dr = [-1,+1,0,0]
+    dc = [0,0,-1,+1]
+    
+    visited[0][0] = True
+    dist[0][0] = 1
+    q = deque([start])
     
     while q:
-        x, y, idx = q.popleft()
+        x, y = q.popleft()
         
-        if x == n-1 and y == m-1:
-            return idx
+        for i in range(4):
+            dx = x + dr[i]
+            dy = y + dc[i]
         
-        for dx, dy in directions:
-            nx = x + dx
-            ny = y + dy
-            
-            if 0 <= nx < n and 0 <= ny < m:
-                if maps[nx][ny] == 1 and not visited[nx][ny]:
-                    visited[nx][ny] = True
-                    q.append((nx,ny,idx+1))
-    
+            if 0 <= dx < n and 0 <= dy < m:
+                if visited[dx][dy] == False and maps[dx][dy] == 1:
+                    visited[dx][dy] = True
+                    q.append((dx, dy))
+                    dist[dx][dy] = dist[x][y] + 1
+        
+        if (x,y) == goal:
+            return dist[x][y]
+                
     return -1
